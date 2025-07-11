3836d7d79abaf74266ff0167a5ec0de931721417
gerardoRmrz <grmzrso@outlook.com>
2025-07-11 16:19:00 -0500

    commit for the exercise 3.2 Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,6 +31,26 @@ app.get('/api/persons', (request, response) => {
 })
 
 
+app.get('/api/info', (request, response) => {
+    const currentTime = new Date()
+    const daysOfWeek = [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ]
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    const dayName = daysOfWeek[currentTime.getDay()]
+    const monthName = months[ currentTime.getMonth() ]
+    const dayOfMonth = currentTime.getDate()
+    const fullYear = currentTime.getFullYear()
+    const hour = currentTime.getHours()
+    const minutes = currentTime.getMinutes()
+    const seconds = currentTime.getSeconds()
+    const timeZone = 100*currentTime.getTimezoneOffset()/60 
+    const timeZoneString = timeZone < 0 ? '+'+`${timeZone}`.padStart(4,'0') : '-'+ `${timeZone}`.padStart(4,'0')
+
+    const dateString = `${dayName} ${monthName} ${dayOfMonth} ${fullYear} ${hour}:${minutes}:${seconds} GMT${timeZoneString}`
+    const timeZoneName = '(Peruvian Standard Time)'
+    
+    response.send(`<p>Phonebook has info for ${persons.length}  people</p>
+        <p>${dateString} ${timeZoneName}</p>`)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
