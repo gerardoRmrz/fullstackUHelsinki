559448d28ec8c3b221277dd639113dd6aeb39c5c
gerardoRmrz <grmzrso@outlook.com>
2025-12-24 09:02:44 -0500

    Se añadieron pruebas para la función total_likes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length===0){
+    return 0
+  }
+  
+  return blogs.reduce( (acc, curr) => { 
+      return acc + curr.likes 
+    }, 0 )
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
