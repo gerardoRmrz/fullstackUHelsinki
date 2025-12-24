4ab89544559746e4d6087f247bb2846e788adc2b
gerardoRmrz <grmzrso@outlook.com>
2025-12-23 19:42:15 -0500

    Se separaron las rutas y la aplicación en módulos separados

---
+++ app.js
@@ -0,0 +1,22 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogApp')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+console.log('Connecting to', config.MONGO_URI)
+
+mongoose.connect(config.MONGO_URI)
+  .then( () => {
+    console.log('connected to MONGO DB')
+  } )
+
+  app.use(cors())
+  app.use(express.json())
+  app.use('/api/blogs', blogsRouter)
+
+  module.exports = app
+
