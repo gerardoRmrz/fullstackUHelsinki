b55f41344afd261dd5b8e6ce01c13a86c6b3cd05
gerardoRmrz <grmzrso@outlook.com>
2025-12-29 10:40:05 -0500

    Se añadió la inicialización de la base de datos para cada prueba para consistencia

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -1,9 +1,19 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const Blog = require('../models/blog')
+const initialBlogs = require('./blogsList')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+
+     beforeEach( async () => {
+        await Blog.deleteMany({})
+        for ( const blog of initialBlogs ) {
+          let blogObject = new Blog( blog )
+          await blogObject.save()
+        }
+    }) 
 
 test.only('blogs are returned as json', async () => {
   await api
