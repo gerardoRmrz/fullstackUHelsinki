eab270f030fc90d7d0e285e13649e79f9988f6ad
gerardoRmrz <grmzrso@outlook.com>
2025-12-30 14:02:27 -0500

    Se agregaron restricciones en la definición del esquema de BD para el username y password

--- models/user.js
+++ models/user.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  name: String,
+  username: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+  name: {
+    type: String,
+    required: true
+  },
   passwordHash: String,
   blogs: [
     {
