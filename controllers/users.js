8656f1fd174313208caccb14a4ffe9010f355c85
gerardoRmrz <grmzrso@outlook.com>
2025-12-30 13:43:10 -0500

    Se agregaron restricciones para el username y password

--- controllers/users.js
+++ controllers/users.js
@@ -2,8 +2,34 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+const checkStringLength = (string, N) => {
+  return string.length >= N
+}
+
+const checkUsernameLength = (username) => {
+  return checkStringLength(username, 3)
+}
+
+const checkUserNameUnique = (username) => {
+  const isUnique = User.find( {username} )
+  return isUnique? false: true
+}
+
+const checkPasswordLength = (passw) => {
+  return checkStringLength(passw, 3)
+}
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
+
+  if ( !checkUsernameLength(username) || !checkPasswordLength(password)) {
+    return response.status(400).json( {error: 'The user name and password should be at least 3 chars long'} )
+  }
+
+  if ( !checkUserNameUnique(username) ) {
+    return response.status(400).json({error: 'The user name should be unique'})
+  }
+
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
