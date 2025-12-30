89d355e73766b0b1061b17dd6cb3dc0eebbc2eda
gerardoRmrz <grmzrso@outlook.com>
2025-12-30 11:37:44 -0500

    se añadió el esquema User y se relacionó con el esquema Blog

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = required('mongoose')
+
+const userSchema = new mongoose.userSchema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]
+})
+
+userSchema.set('toJson', {
+  transform: (document, returnedDocument) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
