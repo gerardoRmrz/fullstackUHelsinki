bf0fcc361cbc88ce2b8c7fcb653b7f7753f3e22f
gerardoRmrz <grmzrso@outlook.com>
2026-01-05 17:52:03 -0500

    Se agregó la funcionalidad de autenticación

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/appRouter')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
   app.use(express.json())
   app.use('/api/users', usersRouter)
   app.use('/api/blogs', blogsRouter)
+  app.use('/api/login', loginRouter)
 
   module.exports = app
 
