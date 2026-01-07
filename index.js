be704ab3109b3030089f49960e3cb8ca8dcd21e4
gerardoRmrz <grmzrso@outlook.com>
2026-01-07 11:11:05 -0500

    Se agregó la ruta para añadir nuevos registros a la agenda telefónica

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -52,6 +54,26 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter( p => p.id!==id )
   
   response.status(204).end()
+})
+
+const getRandomID = () => {
+  const n = 100000000
+  return Math.floor( n*Math.random() ) +1  
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: getRandomID()
+  }
+
+  persons = persons.concat(newPerson)
+
+  response.json(newPerson)
+
 })
 
 
